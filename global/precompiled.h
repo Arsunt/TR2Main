30c882f0596c76c45a3e9003e5d1707c50e15355
Michael Chaban <arsunt@gmail.com>
2020-04-12 19:18:28 +0300

    feature defines moved from the header file to the project file

--- global/precompiled.h
+++ global/precompiled.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2017-2018 Michael Chaban. All rights reserved.
+ * Copyright (c) 2017-2020 Michael Chaban. All rights reserved.
  * Original game is written by Core Design Ltd. in 1997.
  * Lara Croft and Tomb Raider are trademarks of Square Enix Ltd.
  *
@@ -22,7 +22,6 @@
 #ifndef TR2MAIN_PRECOMPILED_HEADER
 #define TR2MAIN_PRECOMPILED_HEADER
 
-#include "features.h"
 #include <stdio.h>
 #include <windows.h>
 #include <shlobj.h>
